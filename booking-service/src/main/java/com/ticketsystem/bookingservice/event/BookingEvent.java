2d1985b51ca7ee03e0592d33f4f362f8ab450eae
Bareera Qureishi <bareera.qureishi@inadev.com>
2026-07-20 13:28:35 +0530

    Update booking-service: switch DB creds to env vars, add Kafka config

--- booking-service/src/main/java/com/ticketsystem/bookingservice/event/BookingEvent.java
+++ booking-service/src/main/java/com/ticketsystem/bookingservice/event/BookingEvent.java
@@ -13,7 +13,7 @@ import java.math.BigDecimal;
 @Builder
 public class BookingEvent {
     private Long userId;
-    private Long EventId;
+    private Long eventId;
     private Long ticketCount;
     private BigDecimal totalPrice;
 }
--- booking-service/src/main/java/com/ticketsystem/bookingservice/service/BookingService.java
+++ booking-service/src/main/java/com/ticketsystem/bookingservice/service/BookingService.java
@@ -2,24 +2,33 @@ package com.ticketsystem.bookingservice.service;
 
 import com.ticketsystem.bookingservice.client.InventoryServiceClient;
 import com.ticketsystem.bookingservice.entity.Customer;
+import com.ticketsystem.bookingservice.event.BookingEvent;
 import com.ticketsystem.bookingservice.repository.CustomerRepository;
 import com.ticketsystem.bookingservice.request.BookingRequest;
 import com.ticketsystem.bookingservice.response.BookingResponse;
 import com.ticketsystem.bookingservice.response.InventoryResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+
 @Service
+@Slf4j
 public class BookingService {
 
     private final CustomerRepository customerRepository;
     private final InventoryServiceClient inventoryServiceClient;
+    private final KafkaTemplate<String, BookingEvent> kafkaTemplate;
 
     @Autowired
     public BookingService(final CustomerRepository customerRepository,
-                          final InventoryServiceClient inventoryServiceClient) {
+                          final InventoryServiceClient inventoryServiceClient,
+                          final KafkaTemplate<String, BookingEvent> kafkaTemplate) {
         this.customerRepository = customerRepository;
         this.inventoryServiceClient = inventoryServiceClient;
+        this.kafkaTemplate = kafkaTemplate;
     }
 
     public BookingResponse createBooking(final BookingRequest request){
@@ -30,12 +39,33 @@ public class BookingService {
         }
         //check if there's enough inventory
         final InventoryResponse inventoryResponse = inventoryServiceClient.getInventory(request.getEventId());
-        System.out.println("Inventory Service Response" + inventoryResponse);
-
-        //get event & venue info
+        log.info("Inventory Response: {}", inventoryResponse);
+        if(inventoryResponse.getCapacity() < request.getTicketCount()) {
+            throw new RuntimeException("Not enough inventory");
+        }
 
         //create booking
-        //send booking to Order service on a Kafka topic
-        return BookingResponse.builder().build();
+        final BookingEvent bookingEvent = createBookingEvent(request, customer, inventoryResponse);
+
+        //send booking to Order service on a Kafka topic:
+        kafkaTemplate.send("booking", bookingEvent);
+        log.info("Booking sent to Kafka: {}", bookingEvent);
+        return BookingResponse.builder()
+                .userId(bookingEvent.getUserId())
+                .eventId(bookingEvent.getEventId())
+                .ticketCount(bookingEvent.getTicketCount())
+                .totalPrice(bookingEvent.getTotalPrice())
+                .build();
+    }
+
+    private BookingEvent createBookingEvent(final BookingRequest request,
+                                            final Customer customer,
+                                            final InventoryResponse inventoryResponse) {
+        return BookingEvent.builder()
+                .userId(customer.getId())
+                .eventId(request.getEventId())
+                .ticketCount(request.getTicketCount())
+                .totalPrice(inventoryResponse.getTicketPrice().multiply(BigDecimal.valueOf(request.getTicketCount())))
+                .build();
     }
 }
